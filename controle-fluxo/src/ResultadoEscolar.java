d5cb14f765833263c0052acde0f5b1023ee60604
mros999 <marcofilho978@gmail.com>
2024-06-07 07:51:28 -0300

    Estruturas de repetição

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,21 @@
+public class ResultadoEscolar {
+public static void main(String[] args) {
+    int nota = 6;
+
+
+    String resultado = nota >= 7 ?"Aprovado" : nota>= 5 && nota < 7? "Recuperação" : "Reprovado";
+    System.out.println(resultado);
+/*
+    if(nota >=7){
+        System.out.println("Aprovado");
+    }
+    else if(nota>= 5 && nota < 7){
+        System.out.println("Nota de recuperação");
+    }
+    else{
+        System.out.println("Reprovado");
+    }
+  */ 
+}
+
+}
